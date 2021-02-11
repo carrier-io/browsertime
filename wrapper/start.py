d6be2864e652e5158da6db94a2444bf5dc6833f8
mykhailo_hunko <mykhailo.hunko@epam.com>
2021-02-11 15:21:48 +0200

    Fix typo header -> headers

--- wrapper/start.py
+++ wrapper/start.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import zipfile
+import logging
 
 from os import environ, path, walk, sep
 from traceback import format_exc
@@ -17,13 +18,13 @@ ITERATIONS = environ.get("tests", "1")
 
 parser = argparse.ArgumentParser(description='Browsertime Args Parser')
 parser.add_argument('--browser', '-b', type=str, nargs="?", default='chrome', help='Broser to be tested, chrome and firefox are allowed')
-parser.add_argument('--header', '-H', type=str, nargs="*", help='Headers in construct of KEY:VALUE')
+parser.add_argument('--headers', '-H', type=str, nargs="*", help='Headers in construct of KEY:VALUE')
 parser.add_argument('url', type=str, help='URL to be tested')
 
 def get_headers():
     if TOKEN:
         return {'Authorization': 'Bearer {}'.format(TOKEN)}
-    logger.warning("=====> Auth TOKEN is not set!")
+    logging.warning("=====> Auth TOKEN is not set!")
     return None
 
 def upload_artifacts(file_path):
@@ -52,7 +53,7 @@ def zipdir(p, zf):
 def run():
     args = parser.parse_args()
     exec_string = '/start.sh --useSameDir --visualMetrics true --visuaElements true -o {} --skipHar -b {} --viewPort {} -n {} '.format(NAME, args.browser, VIEWPORT, int(ITERATIONS))
-    if args.header:
+    if args.headers:
         exec_string += '-r {} '.format(' -r '.join(args.headers))
     exec_string += args.url
     p = Popen(exec_string, stdout=PIPE, stderr=PIPE, shell=True)
