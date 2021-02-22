76abc6221241eb631ec8776a10d1db71fdef40af
Artem Rozumenko <artyom.rozumenko@gmail.com>
2021-02-22 09:44:42 +0200

    added loki logging

--- wrapper/start.py
+++ wrapper/start.py
@@ -2,11 +2,16 @@ import argparse
 import requests
 import zipfile
 import logging
+import logging_loki
+from multiprocessing import Queue
 
 from os import environ, path, walk, sep
 from traceback import format_exc
 
 from subprocess import Popen, PIPE
+
+logger = logging.getLogger("browsertime")
+
 
 GALLOPER_URL = environ.get("galloper_url", None)
 TOKEN = environ.get("token", None)
@@ -15,11 +20,24 @@ NAME = environ.get("filename")
 PROJECT = environ.get("project_id")
 VIEWPORT = environ.get("view", "1920x1080")
 ITERATIONS = environ.get("tests", "1")
+LOG_LEVEL = environ.get("log_level", "info")
+
+if GALLOPER_URL:
+    handler = logging_loki.LokiQueueHandler(
+        Queue(-1),
+        url=f"{GALLOPER_URL.replace('https://', 'http://')}:3100/loki/api/v1/push",
+        tags={"application": "interceptor"},
+        version="1",
+    )
+
+    logger.setLevel(logging.INFO if LOG_LEVEL == 'info' else logging.DEBUG)
+    logger.addHandler(handler)
 
 parser = argparse.ArgumentParser(description='Browsertime Args Parser')
 parser.add_argument('--browser', '-b', type=str, nargs="?", default='chrome', help='Broser to be tested, chrome and firefox are allowed')
 parser.add_argument('--headers', '-H', type=str, nargs="*", help='Headers in construct of KEY:VALUE')
 parser.add_argument('url', type=str, help='URL to be tested')
+
 
 def get_headers():
     if TOKEN:
@@ -27,15 +45,17 @@ def get_headers():
     logging.warning("=====> Auth TOKEN is not set!")
     return None
 
+
 def upload_artifacts(file_path):
     try:
         file = {'file': open(file_path, 'rb')}
         requests.post('{}/api/v1/artifacts/{}/{}/{}.json'.format(GALLOPER_URL, PROJECT, BUCKET, NAME),
                       files=file,
                       headers=get_headers())
+        logging.info("Upload Successful")
     except Exception:
         from time import sleep
-        print(format_exc())
+        logging.error(format_exc())
         sleep(120)
 
 
@@ -50,6 +70,7 @@ def zipdir(p, zf):
                 _p = ""
             zf.write(path.join(root, file), arcname=path.join(_p, file))
 
+
 def run():
     args = parser.parse_args()
     exec_string = '/start.sh --useSameDir --visualMetrics true --visuaElements true -o {} --skipHar -b {} --viewPort {} -n {} '.format(NAME, args.browser, VIEWPORT, int(ITERATIONS))
@@ -57,7 +78,9 @@ def run():
         exec_string += '-r {} '.format(' -r '.join(args.headers))
     exec_string += args.url
     p = Popen(exec_string, stdout=PIPE, stderr=PIPE, shell=True)
-    p.communicate()
+    res = p.communicate()
+    logging.info(f"Execution result: {res[0]}")
+    logging.info(f"Execution error: {res[1]}")
     zf = zipfile.ZipFile('/browsertime/{}.zip'.format(NAME), 'w', zipfile.ZIP_DEFLATED)
     zipdir('browsertime-results/', zf)
     zf.close()
